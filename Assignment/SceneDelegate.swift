45752c3bae201898ef096f6f06f80d06d7541252
Gagan Verma <vermagagan1512@gmail.com>
2026-05-15 11:03:39 +0530

    Minor changes

--- Assignment/SceneDelegate.swift
+++ Assignment/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
             guard let windowScene = (scene as? UIWindowScene) else { return }
             window = UIWindow(windowScene: windowScene)
-            window?.rootViewController = ViewController() // your initial VC
+            window?.rootViewController = UINavigationController(rootViewController: CountryListVC()) // your initial VC
             window?.makeKeyAndVisible()
         }
 
